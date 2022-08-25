ee534663577739ac70a2a70612702a096f25a688
Sean Kennedy <snkennedy21@gmail.com>
2022-08-25 17:48:22 -0400

    add List and Detail GET requests for Attendees, Locations, Presentations, and Conferences

--- events/api_views.py
+++ events/api_views.py
@@ -4,24 +4,6 @@ from .models import Conference, Location
 
 
 def api_list_conferences(request):
-    """
-    Lists the conference names and the link to the conference.
-
-    Returns a dictionary with a single key "conferences" which
-    is a list of conference names and URLS. Each entry in the list
-    is a dictionary that contains the name of the conference and
-    the link to the conference's information.
-
-    {
-        "conferences": [
-            {
-                "name": conference's name,
-                "href": URL to the conference,
-            },
-            ...
-        ]
-    }
-    """
     response = []
     conferences = Conference.objects.all()
     for conference in conferences:
@@ -35,30 +17,6 @@ def api_list_conferences(request):
 
 
 def api_show_conference(request, pk):
-    """
-    Returns the details for the Conference model specified
-    by the pk parameter.
-
-    This should return a dictionary with the name, starts,
-    ends, description, created, updated, max_presentations,
-    max_attendees, and a dictionary for the location containing
-    its name and href.
-
-    {
-        "name": the conference's name,
-        "starts": the date/time when the conference starts,
-        "ends": the date/time when the conference ends,
-        "description": the description of the conference,
-        "created": the date/time when the record was created,
-        "updated": the date/time when the record was updated,
-        "max_presentations": the maximum number of presentations,
-        "max_attendees": the maximum number of attendees,
-        "location": {
-            "name": the name of the location,
-            "href": the URL for the location,
-        }
-    }
-    """
     conference = Conference.objects.get(id=pk)
     return JsonResponse(
         {
@@ -79,42 +37,23 @@ def api_show_conference(request, pk):
 
 
 def api_list_locations(request):
-    """
-    Lists the location names and the link to the location.
-
-    Returns a dictionary with a single key "locations" which
-    is a list of location names and URLS. Each entry in the list
-    is a dictionary that contains the name of the location and
-    the link to the location's information.
-
-    {
-        "locations": [
-            {
-                "name": location's name,
-                "href": URL to the location,
-            },
-            ...
-        ]
-    }
-    """
-    return JsonResponse({})
+    response = []
+    locations = Location.objects.all()
+    for location in locations:
+        response.append({
+            "name": location.name,
+            "href": location.get_api_url(),
+        })
+    return JsonResponse({"locations": response})
 
 
 def api_show_location(request, pk):
-    """
-    Returns the details for the Location model specified
-    by the pk parameter.
-
-    This should return a dictionary with the name, city,
-    room count, created, updated, and state abbreviation.
-
-    {
-        "name": location's name,
-        "city": location's city,
-        "room_count": the number of rooms available,
-        "created": the date/time when the record was created,
-        "updated": the date/time when the record was updated,
-        "state": the two-letter abbreviation for the state,
-    }
-    """
-    return JsonResponse({})
+    location = Location.objects.get(id=pk)
+    return JsonResponse({
+        "name": location.name,
+        "city": location.city,
+        "room_count": location.room_count,
+        "created": location.created,
+        "updated": location.updated,
+        "state": location.state.abbreviation,
+    })
